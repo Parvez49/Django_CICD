744c2b5616666febf702aa472d9af12f45f8781c
parvezhossen <parvez@merchantbay.com>
2024-08-31 13:21:26 +0600

    simple hello world api added

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from .views import HelloWorld
+
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("hello",HelloWorld.as_view(),name='hello')
 ]
